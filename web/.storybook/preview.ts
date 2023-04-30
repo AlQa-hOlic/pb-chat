c44e8d87d631c1c7faca018674489c3e6ec8ebf4
Rohit Krishnan <rohit.k.kesavan@gmail.com>
2023-04-30 12:12:37 +0530

    feat[web]: add storybook

---
+++ web/.storybook/preview.ts
@@ -0,0 +1,20 @@
+import type { Preview } from "@storybook/react";
+
+import "../src/index.css";
+
+const preview: Preview = {
+  parameters: {
+    // layout: "fullscreen",
+    layout: "centered",
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    // backgrounds: { disable: true },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <pre>Hello, world!</pre>;
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center bg-orange-500">
+      <pre className="text-6xl text-white">Hello, world</pre>
+    </main>
+  );
 }
 
 export default App;
