dfb02a94a9013bc5e71a090242f14e53a13b6efc
Rohit Krishnan <rohit.k.kesavan@gmail.com>
2023-04-30 15:18:44 +0530

    chore[web]: fix formatting

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <main className="flex min-h-screen w-full items-center justify-center bg-orange-500">
       <pre className="text-6xl text-white">Hello, world</pre>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
